3bb6421e54094d923236aecb3f5af26dada6fcb8
zhangwanyu <zhangwanyu@kingsoft.com>
2017-11-01 21:34:30 +0800

    update

--- src/com/example/myapp/function/TreeUtility.java
+++ src/com/example/myapp/function/TreeUtility.java
@@ -7,13 +7,12 @@ public class TreeUtility {
 
     public static class Node {
         private int Data;// 数据域
-        private Node Next;// 指针域
-        private Node Special;//特殊指向
+        private Node mLeft = null;// 左子树
+        private Node mRight = null;//右子树
         public Node() {
 
         }
         public Node(int Data) {
-            // super();
             this.Data = Data;
         }
         public int getData() {
@@ -23,18 +22,29 @@ public class TreeUtility {
             this.Data = Data;
         }
 
-        public Node getNext() {
-            return Next;
+        public Node getLeft() {
+            return mLeft;
         }
-        public void setNext(Node Next) {
-            this.Next = Next;
+        public void setLeft(Node Next) {
+            this.mLeft = Next;
         }
 
-        public Node getSpecial(){
-            return  Special;
+        public Node getRight(){
+            return  mRight;
         }
-        public void setSpecial(Node special){
-            this.Special = special;
+        public void setRight(Node special){
+            this.mRight = special;
         }
+    }
+
+    private static Node initTree() {
+        Node root = new Node(1);
+        Node node2 = new Node(2);Node node3 = new Node(3);
+        Node node4 = new Node(4);Node node5 = new Node(5);Node node6 = new Node(6);Node node7 = new Node(7);
+
+        root.setLeft(node2); root.setRight(node3);
+        node2.setLeft(node4);node2.setRight(node5);node3.setLeft(node6);node3.setRight(node7);
+
+        return root;
     }
 }
